3bcfb55187a4e7800384de0b9635282379ca4ded
Brendan MacDonald <brendan.macdonald@cameronandwilding.com>
2016-01-25 10:04:38 +0000

    Update to composer scripts and bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,6 @@
 #  Get Selenium Server.
 wget http://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.1.jar -O Servers/selenium.jar
 
-#  Install Behat via Composer.
-#export COMPOSER_HOME="./"
-#composer install -d Behat
-#rm -rf Behat/.htaccess
 
 #  Create a local behat configuration file.
 cat > Behat/behat.local.yml << EOF
@@ -18,6 +14,7 @@ default:
       drupal:
         drupal_root: /Applications/MAMP/htdocs/
 EOF
+
 
 #  Rename the 'Sample_Files' directory so that does not get updated by composer update.
 mv Sample_Files Project_Files
