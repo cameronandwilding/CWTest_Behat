a2268af91e893b472823680d63d98e9e3aae6e14
Brendan MacDonald <brendan.macdonald@cameronandwilding.com>
2016-01-25 09:41:13 +0000

    Update to composer dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,9 @@
 wget http://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.1.jar -O Servers/selenium.jar
 
 #  Install Behat via Composer.
-export COMPOSER_HOME="./"
-composer install -d Behat
-rm -rf Behat/.htaccess
+#export COMPOSER_HOME="./"
+#composer install -d Behat
+#rm -rf Behat/.htaccess
 
 #  Create a local behat configuration file.
 cat > Behat/behat.local.yml << EOF
