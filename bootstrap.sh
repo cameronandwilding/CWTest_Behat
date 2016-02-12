4303eb60421bb54ef4cd0e6888d5b5ad95246cfd
Eric Smith <eric.smith@cameronandwilding.com>
2016-02-12 16:13:24 +0000

    Delete old files

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,11 @@ TEST_DIR=./../../..
 SERVER_DIR=./../../../Servers
 RESULTS_DIR=./../../../Results
 
-mkdir ${TEST_DIR}
 mkdir ${SERVER_DIR}
 mkdir ${RESULTS_DIR}
 
 # Copy the sample files over to the main directory.
-cp -R Sample_Files/* ${BEHAT_DIR}
+cp -R Sample_Files/* ${TEST_DIR}
 
 # Get Selenium Server.
 wget http://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.1.jar -O ${SERVER_DIR}/selenium.jar
