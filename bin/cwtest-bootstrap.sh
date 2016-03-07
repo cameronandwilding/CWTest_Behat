0aa6ef517ff12a3d7835b6e9589055744e8b7c2f
Gareth Goodwin <gareth@fivemilemedia.co.uk>
2016-03-07 20:18:03 +0000

    Adjusted the ./bin/cwtest-bootstrap.sh script so that it will try to download selenium via wget or curl, before providing the user manual instructions.

--- bin/cwtest-bootstrap.sh
+++ bin/cwtest-bootstrap.sh
@@ -7,7 +7,15 @@ mkdir ./../Results/Behat
 mkdir ./../Results/Behat/screenshots
 
 # Download selenium
-wget https://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar -O ./../Servers/selenium.jar
+SELENIUM_URL="https://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar"
+SELENIUM_DESTINATION="./../Servers/selenium.jar"
+if type wget -v >/dev/null 2>&1; then
+  wget ${SELENIUM_URL} -O ${SELENIUM_DESTINATION}
+elif type curl -V >/dev/null 2>&1; then
+  curl ${SELENIUM_URL} -O ${SELENIUM_DESTINATION}
+else
+  echo "Unable to download Selenium. Please download it from ${SELENIUM_URL} and place it in ${SELENIUM_DESTINATION}"
+fi
 
 # Copy over the sample files directory.
 BIN_DIR="$(dirname $(readlink $BASH_SOURCE))"
