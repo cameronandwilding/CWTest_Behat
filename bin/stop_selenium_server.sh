30ec2a7924d235691fd8f34f29b308e73b6ec23f
kenorb <kenorb@users.noreply.github.com>
2017-06-27 13:03:20 +0100

    Fail script on error

--- bin/stop_selenium_server.sh
+++ bin/stop_selenium_server.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
 
-#  Stop Selenium Server
+# Stop Selenium Server.
 printf "\nStopping selenium server...\n"
 cURL http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer
-
-
-
