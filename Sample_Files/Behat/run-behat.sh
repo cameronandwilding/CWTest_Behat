8bac6d7c20c0579e0e79a774c5b78e8026ea487b
kenorb <kenorb@users.noreply.github.com>
2017-07-18 17:37:46 +0100

    Corrects shell scripts to run context independent (GH-46)

--- Sample_Files/Behat/run-behat.sh
+++ Sample_Files/Behat/run-behat.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 ##############################################################################
-###    BACKUP PREVIOUS RESULT FILES
-##############################################################################
-mkdir -p ../Results/History
-ls ../Results/*.html && mv ../Results/*.html ../Results/History
-
-
-##############################################################################
 ###    GLOBAL VARS
 ##############################################################################
-COMPOSER_BIN=../bin
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+COMPOSER_BIN="$CWD"/../bin
+
+##############################################################################
+###    BACKUP PREVIOUS RESULT FILES
+##############################################################################
+mkdir -p "$CWD"/../Results/History
+ls "$CWD"/../Results/*.html && mv "$CWD"/../Results/*.html "$CWD"/../Results/History
 
 ##############################################################################
 ###    ASSIGN SCRIPT VARIABLES
@@ -18,7 +18,6 @@ COMPOSER_BIN=../bin
 TAG=$1
 PROFILE=$2
 SCENARIO_NAME=$3
-
 
 ##############################################################################
 ###    HELP OPTION
@@ -31,7 +30,6 @@ if [ "$1" == "-h" ]; then
   exit 0
 fi
 
-
 ##############################################################################
 ###    SHELL SCRIPT MUST ALWAYS BE PASSED THE TAG AND PROFILE VARIABLES
 ##############################################################################
@@ -40,7 +38,6 @@ then
    printf 'ERROR: Expected Tag followed by Profile.\nE.g. ./run-behat.sh [tag] [profile]\n'
    exit 1
 fi
-
 
 ##############################################################################
 ###    TEST EXECUTION
@@ -54,9 +51,9 @@ then
    fi
    if [ ! -z "$SCENARIO_NAME" ]
    then
-      $COMPOSER_BIN/behat -c behat.yml --tags=@$TAG -p $PROFILE --name="$SCENARIO_NAME" ${@:4}
+      "$COMPOSER_BIN"/behat -c "$CWD"/behat.yml --tags=@$TAG -p $PROFILE --name="$SCENARIO_NAME" ${@:4}
    else
-      $COMPOSER_BIN/behat -c behat.yml --tags=@$TAG -p $PROFILE ${@:4}
+      "$COMPOSER_BIN"/behat -c "$CWD"/behat.yml --tags=@$TAG -p $PROFILE ${@:4}
    fi
 fi
 
@@ -66,9 +63,8 @@ then
      printf 'ERROR: Failed to run PhantomJS web driver!\n'
      exit 1
    fi
-   $COMPOSER_BIN/behat --tags=@$TAG -p $PROFILE ${@:4}
+   "$COMPOSER_BIN"/behat --tags=@$TAG -p $PROFILE ${@:4}
 fi
-
 
 ##############################################################################
 ###    TEARDOWN
@@ -76,11 +72,11 @@ fi
 #  Stop PhantomJS webdriver.
 if [ $PROFILE = "phantomjs" ]
 then
-   sh $COMPOSER_BIN/stop_phantomjs_webdriver.sh
+   . "$COMPOSER_BIN"/stop_phantomjs_webdriver.sh
 fi
 
 #  Stop Selenium server.
 if [ $PROFILE = "firefox" ] || [ $PROFILE = "chrome"  ]
 then
-   . $COMPOSER_BIN/stop_selenium_server.sh
+   . "$COMPOSER_BIN"/stop_selenium_server.sh
 fi
